49b318153029bc28cc4051246896038ee7a80c63
sdhz150 <yanghui27004@gmail.com>
2022-06-19 18:36:13 +0800

    x86/helloworld

---
+++ diy-part5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+echo 'src-git helloworld https://github.com/fw876/helloworld.git;master' >>feeds.conf.default
+
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+svn co https://github.com/kenzok8/small-package/trunk/ipt2socks package/ipt2socks
+svn co https://github.com/kenzok8/small-package/trunk/microsocks package/microsocks
+svn co https://github.com/kenzok8/small-package/trunk/dns2socks package/dns2socks
+svn co https://github.com/kenzok8/small-package/trunk/redsocks2 package/redsocks2
